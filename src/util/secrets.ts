f265655c26591fa64161a0c300ef408b27939fd0
Kenn Pascascio <kenn370@gmail.com>
2018-12-28 21:09:12 -0500

    fixed double quotes for prod build

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -1,23 +1,23 @@
-import logger from "./logger";
-import dotenv from "dotenv";
-import fs from "fs";
+import logger from './logger';
+import dotenv from 'dotenv';
+import fs from 'fs';
 
-if (fs.existsSync(".env")) {
-    logger.debug("Using .env file to supply config environment variables");
-    dotenv.config({ path: ".env" });
+if (fs.existsSync('.env')) {
+    logger.debug('Using .env file to supply config environment variables');
+    dotenv.config({ path: '.env' });
 }
 export const ENVIRONMENT = process.env.NODE_ENV;
-const prod = ENVIRONMENT === "production"; // Anything else is treated as 'dev'
+const prod = ENVIRONMENT === 'production'; // Anything else is treated as 'dev'
 
-export const SESSION_SECRET = process.env["SESSION_SECRET"];
-export const LOCALDBURI = prod ? process.env["LOCALDBURI"] : process.env["LOCALDBURI"];
+export const SESSION_SECRET = process.env['SESSION_SECRET'];
+export const LOCALDBURI = prod ? process.env['LOCALDBURI'] : process.env['LOCALDBURI'];
 
 if (!SESSION_SECRET) {
-    logger.error("No client secret. Set SESSION_SECRET environment variable.");
+    logger.error('No client secret. Set SESSION_SECRET environment variable.');
     process.exit(1);
 }
 
 if (!LOCALDBURI) {
-    logger.error("No mongo connection string. Set LOCALDBURI environment variable.");
+    logger.error('No mongo connection string. Set LOCALDBURI environment variable.');
     process.exit(1);
 }
